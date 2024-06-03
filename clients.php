12b7745478ebbdc8c54ffff43b744c7d82dff672
Kendal <kendal3@hotmail.fr>
2024-06-03 13:19:20 +0200

    add modification for  Nos Client

--- clients.php
+++ clients.php
@@ -8,36 +8,58 @@ require_once 'templates/header.php';
 
 <body>
 
-<h1 style="text-align:center; padding:20px;">Votre satisfaction, notre passion. Ensemble, créons l’extraordinaire.</h1>
+<h1 style="text-align:center; padding:50px; color: #1e4f83;">Votre satisfaction, notre passion. Ensemble, créons l’extraordinaire.</h1>
+
+<div class="review" data-aos="fade-up" data-aos-duration="1000">
+    <img src="/upload/review.jpeg" alt="">
+</div>
 
 
-<div style="width: 60%; margin: auto;">
+
+
+<div style="width: 85%; margin: auto; color: #1e4f83; font-family:'Times New Roman', Times, serif; font-size: 25px; margin-top: -130px;">
   <p style="text-align:center; padding:20px;">En quelques années, le <strong>Groupe VDMH</strong> a su développer avec ses clients une relation pérenne basée sur la confiance, le respect des engagements et une connaissance approfondie de leurs métiers, de leurs organisations et de leurs systèmes d'information. Fort de cette crédibilité, le groupe déploie aujourd'hui sa gamme de services auprès des banques & institutions de premier rang.</p>
 </div>
 
-<div class="container-cl">
+<div class="container-cl" style="margin-top: 100px;" >
   <div class="image-text">
-    <img src="/upload/mdp.jpeg" alt="Image 1" width="250" height="100">
-    <p> Nous avons collaborer avec HSBC une des plus grandes banque d'europ au MONDE</p>
+    <img src="/upload/mdp.jpeg" alt="Image 1" width="250" height="100" data-aos="fade-right">
+    <p data-aos="fade-left" style= "color: #1e4f83; font-family:'Times New Roman', Times, serif; font-size: 20px;">Nous avons collaborer avec HSBC une des plus grandes banque d'europ au MONDE</p>
   </div>
   <div class="image-text">
-    <p>Nous avons collaborer avec LCL une des plus grandes banque d'europ au MONDE</p>
-    <img src="/upload/wlm.jpeg" alt="Image 2" width="190" height="100">
+    <p data-aos="fade-right" style= "color: #1e4f83; font-family:'Times New Roman', Times, serif; font-size: 20px;">Nous avons collaborer avec LCL une des plus grandes banque d'europ au MONDE</p>
+    <img src="/upload/wlm.jpeg" alt="Image 2" width="190" height="100" data-aos="fade-left">
   </div>
   <div class="image-text">
-    <img src="/upload/frtv.jpeg" alt="Image 3" width="190" height="100">
-    <p>Nous avons collaborer avec BNP une des plus grandes banque d'europ au MONDE</p>
+    <img src="/upload/frtv.jpeg" alt="Image 3" width="190" height="100" data-aos="fade-right">
+    <p data-aos="fade-left" style= "color: #1e4f83; font-family:'Times New Roman', Times, serif; font-size: 20px;">Nous avons collaborer avec BNP une des plus grandes banque d'europ au MONDE</p>
     </div>
   <div class="image-text">
-    <p>Nous avons collaborer avec CIC une des plus grandes banque d'europ au MONDE</p>
-    <img src="/upload/ntx.jpeg" alt="Image 4" width="200" height="100">
+    <p data-aos="fade-right" style= "color: #1e4f83; font-family:'Times New Roman', Times, serif; font-size: 20px;">Nous avons collaborer avec CIC une des plus grandes banque d'europ au MONDE</p>
+    <img src="/upload/ntx.jpeg" alt="Image 4" width="200" height="100" data-aos="fade-left">
   </div>
+
   
 </div>
 
-<div class="marquee" style="margin-bottom: 50px;">
-<p><img src="/upload/ntx.jpeg" alt="Logo 1" style="max-height: 150px;"> <img src="/upload/frtv.jpeg" alt="Logo 2" style="max-height: 150px;"> <img src="/upload/mdp.jpeg" alt="Logo 3" style="max-height: 150px;"><img src="/upload/lcl.jpeg" alt="Logo 4" style="max-height: 150px;"></p>
+<h1 style="text-align:center; padding:20px; margin-top: 70px; color: #1e4f83;">Il nous ont fait confiance : </h1>
+
+<div class="marquee">
+    <div id="images">
+        <img src="/upload/ntx.jpeg" alt="Logo 1" style="max-height: 150px;"> 
+        <img src="/upload/frtv.jpeg" alt="Logo 2" style="max-height: 150px;"> 
+        <img src="/upload/mdp.jpeg" alt="Logo 3" style="max-height: 150px;">
+        <img src="/upload/lcl.jpeg" alt="Logo 4" style="max-height: 150px;">
+        <img src="/upload/ml.jpeg" alt="Logo 5" style="max-height: 150px;">
+        <img src="/upload/part.jpeg" alt="Logo 6" style="max-height: 150px;">
+    </div>
 </div>
+
+
+
+
+
+
 
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 </body>
--- script.js
+++ script.js
@@ -21,3 +21,29 @@ AOS.init();
 
 
 
+
+
+window.onload = function() {
+    var imagesDiv = document.getElementById('images');
+    var images = Array.from(imagesDiv.children);
+    var totalWidth = 0;
+
+    // Calculer la largeur totale des images incluant la marge droite de 30px
+    images.forEach(function(img) {
+        totalWidth += img.offsetWidth + 30;
+    });
+
+    // Mettre à jour la largeur du conteneur des images pour inclure deux ensembles d'images
+    imagesDiv.style.width = totalWidth * 2 + 'px';
+
+    // Doublons les images pour créer un effet de défilement continu
+    images.forEach(function(img) {
+        imagesDiv.appendChild(img.cloneNode(true));
+    });
+
+    // Appliquer l'animation via CSS
+    imagesDiv.style.animationDuration = '30s'; // Ajuster cette valeur pour changer la vitesse
+};
+
+
+
